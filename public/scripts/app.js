be98fa0da7478f9b87cffc7ba74441eb8a318b63
Sonia Boller <soniaboller@Sonias-MacBook-Pro.local>
2016-10-21 12:14:10 -0500

    rotating particles

---
+++ public/scripts/app.js
@@ -0,0 +1,97 @@
+console.log('windlow loaded');
+
+var scene, camera, renderer;
+init();
+
+function init(){
+    scene = new THREE.Scene();
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    var fov = 50;
+
+    renderer = new THREE.WebGLRenderer({antialias:true});
+    renderer.setSize(width,height);
+    document.body.appendChild(renderer.domElement);
+
+    camera = new THREE.PerspectiveCamera(fov, width/height, 0.1, 2000);
+    camera.position.set(0,0,500);
+
+    renderer.setClearColor(0x000000, 1);
+    window.addEventListener('resize', function(){
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        renderer.setSize(width, height);
+        camera.aspect = width/height;
+        camera.updateProjectionMatrix();
+    });
+
+    var group = new THREE.Group();
+    scene.add( group );
+    var group2 = new THREE.Group();
+    scene.add( group2 );
+    var group3 = new THREE.Group();
+    scene.add( group3 );
+    var group4 = new THREE.Group();
+    scene.add( group4 );
+
+    //
+    // function hexGenerator(){
+    //     var color
+    // }
+    for (var i = 0; i < 2500; i++){
+        var material = new THREE.SpriteMaterial({color: 0xff00ff});
+        var particle = new THREE.Particle( material );
+        particle.position.x = Math.random() * 250;
+        particle.position.y = Math.random() * 250;
+        particle.position.z = Math.random() * 150;
+        group.add( particle )
+    }
+
+    for (var j = 0; j < 2500; j++){
+        var material2 = new THREE.SpriteMaterial({color: 0xffffff});
+        var particle2 = new THREE.Particle( material2 );
+        particle2.position.x = Math.random() * 250 - 250;
+        particle2.position.y = Math.random() * 250;
+        particle2.position.z = Math.random() * 150;
+        group2.add( particle2 )
+    }
+
+    for (var k = 0; k < 2500; k++){
+        var material3 = new THREE.SpriteMaterial({color: 0xffff00});
+        var particle3 = new THREE.Particle( material3 );
+        particle3.position.x = Math.random() * 250;
+        particle3.position.y = Math.random() * 250 - 250;
+        particle3.position.z = Math.random() * 150;
+        group3.add( particle3 )
+    }
+
+    for (var l = 0; l < 2500; l++){
+        var material4 = new THREE.SpriteMaterial({color: 0x0000ff});
+        var particle4 = new THREE.Particle( material4 );
+        particle4.position.x = Math.random() * 250 - 250;
+        particle4.position.y = Math.random() * 250 - 250;
+        particle4.position.z = Math.random() * 150;
+        group4.add( particle4 )
+    }
+
+
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    function animate(){
+        requestAnimationFrame(animate);
+        group.rotation.y += 0.05;
+        group2.rotation.y += 0.05;
+        group3.rotation.y += 0.05;
+        group4.rotation.y += 0.05;
+        // group.rotation.z += 0.01;
+        // group2.rotation.z += 0.01;
+        // group3.rotation.z += 0.01;
+        // group4.rotation.z += 0.01;
+        renderer.render(scene,camera);
+        controls.update();
+    }
+    animate();
+
+}
+
