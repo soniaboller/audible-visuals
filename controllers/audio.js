36114a882da3b186f7e9e688e3ee498ce63615b4
Sonia Boller <soniaboller@Sonias-MacBook-Pro.local>
2016-11-10 01:13:42 -0600

    added wave visualizer

--- controllers/audio.js
+++ controllers/audio.js
@@ -9,4 +9,8 @@ audioCtrl.get('/lines', function(req, res, next) {
   res.render('lines',{ title: 'Three JS Line Audio Visualization'});
 });
 
+audioCtrl.get('/waveform', function(req, res, next) {
+  res.render('waveform',{ title: 'Three JS Line Audio Visualization'});
+});
+
 module.exports = audioCtrl;
