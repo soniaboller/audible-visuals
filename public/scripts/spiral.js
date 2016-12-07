a280640ec63a47993172a3a38a1356fa9a969b10
Sonia Boller <soniaboller@gmail.com>
2016-12-06 23:09:17 -0600

    real spiral and circle

--- public/scripts/spiral.js
+++ public/scripts/spiral.js
@@ -50,7 +50,26 @@ function init() {
 
     // move this into the particle generating loop for color changing, but prevents bottom tiles from being accessed for rotation
 
-    for (var i = 0; i <= 2048; i++) {
+    // for (var i = 0; i <= 2048; i++) {
+    //     var material = new THREE.SpriteCanvasMaterial({
+    //         color: 0xffffff,
+    //         program: function (context) {
+    //
+    //             context.beginPath();
+    //             context.arc(0, 0, 0.25, 0, PI2, true);
+    //             context.fill();
+    //
+    //         }
+    //     });
+    //     var particle = particles[i++] = new THREE.Particle(material);
+    //     particle.position.x = Math.sin(i) * (i / 75);
+    //     particle.position.y = Math.cos(i) * (i / 75);
+    //     scene.add(particle);
+    // }
+
+    for (var i = 0; i <=2048; i++) {
+        var a = 1;
+        var b = 1;
         var material = new THREE.SpriteCanvasMaterial({
             color: 0xffffff,
             program: function (context) {
@@ -61,9 +80,10 @@ function init() {
 
             }
         });
+        // var angle = 0.03;
         var particle = particles[i++] = new THREE.Particle(material);
-        particle.position.x = Math.sin(i) * (i / 75);
-        particle.position.y = Math.cos(i) * (i / 75);
+        // particle.position.x = (a + b * (angle * i)) * Math.cos((angle * i));
+        // particle.position.y = (a + b * (angle * i)) * Math.sin((angle * i));
         scene.add(particle);
 
     }
@@ -138,11 +158,16 @@ var GuiControls = function(){
     this.toggleRed = true;
     this.toggleGreen = false;
     this.toggleBlue = false;
-    this.fov = 25;
+    this.fov = 35;
     this.R = 0.7;
     this.G = 0;
     this.B = 0.7;
-    this.radius = 65;
+    this.radius = 50;
+    this.a = 0.15;
+    this.b = 0.2;
+    this.angle = 0.11;
+    this.circle = false;
+    this.spiral = true;
 };
 
 var spiral = new GuiControls();
@@ -150,9 +175,20 @@ var spiral = new GuiControls();
 var gui = new dat.GUI();
 console.log(gui);
 gui.closed = true;
+gui.add(spiral, 'spiral').name('Spiral').listen().onChange(function(){
+    spiral.circle = false;
+    spiral.spiral = true;
+});
+gui.add(spiral, 'circle').name('Circle').listen().onChange(function(){
+    spiral.circle = true;
+    spiral.spiral = false;
+});
 gui.add(spiral, 'intensity', 0.05, 1).name('Intensity');
-gui.add(spiral, 'fov', 1, 50).name('Zoom Distance');
-gui.add(spiral, 'radius', 10, 100).name('Particle Spacing');
+gui.add(spiral, 'fov', 1, 150).name('Zoom Distance');
+gui.add(spiral, 'radius', 10, 100).name('Circle Radius');
+gui.add(spiral,'a', 0, 50).step(0.01).name('Spiral Inner Radius');
+gui.add(spiral,'b', 0, 5).step(0.01).name('Spiral Diameter');
+gui.add(spiral,'angle', 0.001, 0.5).step(0.001).name('Spiral Angle');
 // gui.add(spiral, 'toggleColor').name('Toggle Colors');
 // gui.add(spiral, 'emphasis', {Red: true, Green: true, Blue: true});
 gui.add(spiral, 'toggleRed').name('Red Emphasis').listen().onChange(function(){
@@ -181,11 +217,11 @@ folder.open();
 
 function animate() {
     requestAnimationFrame(app.animate);
-    // stats.begin();
+    stats.begin();
     animateParticles();
     camera.lookAt( scene.position );
     renderer.render( scene, camera );
-    // stats.end();
+    stats.end();
 }
 
 function animateParticles(){
@@ -195,8 +231,6 @@ function animateParticles(){
     analyser.getFloatTimeDomainData(timeFloatData);
     for (var j = 0; j <= particles.length; j++){
         particle = particles[j++];
-        particle.position.z = (timeFloatData[j] * timeFrequencyData[j] * spiral.intensity);
-
         if (spiral.toggleRed){
             var R = spiral.R + (timeFloatData[j]);
             var G = spiral.G - (timeFloatData[j]);
@@ -221,8 +255,27 @@ function animateParticles(){
         // if (!app.play){
         //     particle.material.color.setHex(0x000000);
         // }
-        particle.position.x = Math.sin(j) * (j / spiral.radius);
-        particle.position.y = Math.cos(j) * (j / spiral.radius);
+        if (spiral.circle){
+            particle.position.x = Math.sin(j) * (j / (j/spiral.radius));
+            particle.position.y = (timeFloatData[j] * timeFrequencyData[j] * spiral.intensity);
+            particle.position.z = Math.cos(j) * (j / (j/spiral.radius));
+            camera.fov = 35;
+            camera.position.y = 100;
+        }
+        else if (spiral.spiral){
+            // counter clockwise if x = cos and y = sin, clockwise if x = sin and y = cos
+            particle.position.x = (spiral.a + spiral.b * (spiral.angle * j)) * Math.sin((spiral.angle * j));
+            particle.position.y = (spiral.a + spiral.b * (spiral.angle * j)) * Math.cos((spiral.angle * j));
+            particle.position.z = (timeFloatData[j] * timeFrequencyData[j] * spiral.intensity);
+            camera.position.y = 0;
+        }
+        else {
+            particle.position.x = Math.sin(j) * (j / spiral.radius);
+            particle.position.y = Math.cos(j) * (j / spiral.radius);
+            particle.position.z = (timeFloatData[j] * timeFrequencyData[j] * spiral.intensity);
+            camera.position.y = 0;
+        }
+
     }
     camera.fov = spiral.fov;
     camera.updateProjectionMatrix();
