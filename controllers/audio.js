bd447654ba22bc72a793341ed990fd53406e705a
Sonia Boller <soniaboller@gmail.com>
2017-02-17 12:06:00 -0500

    removed unecessary plane files and changed title

--- controllers/audio.js
+++ controllers/audio.js
@@ -2,32 +2,23 @@ var express = require('express');
 var audioCtrl = express.Router();
 
 audioCtrl.get('/waveform', function(req, res, next) {
-  res.render('waveform',{ title: 'WAVVEZ'});
+  res.render('waveform',{ title: 'Wave'});
 });
 
 audioCtrl.get('/', function(req, res, next) {
-  res.render('sphere',{ title: 'SPHERE'});
+  res.render('sphere',{ title: 'Sphere'});
 });
 
 audioCtrl.get('/square', function(req, res, next) {
-  res.render('square',{ title: 'SQUARE'});
+  res.render('square',{ title: 'Square'});
 });
 
 audioCtrl.get('/line', function(req, res, next) {
-  res.render('lines',{ title: 'LINE'});
+  res.render('lines',{ title: 'Line'});
 });
 
 // audioCtrl.get('/', function(req, res, next) {
 //   res.render('spiral',{ title: 'SPIRAL'});
 // });
-
-audioCtrl.get('/test', function(req, res, next) {
-  res.render('test',{ title: 'test'});
-});
-
-
-audioCtrl.get('/plane', function(req, res, next) {
-  res.render('plane',{ title: 'PLANE'});
-});
 
 module.exports = audioCtrl;
